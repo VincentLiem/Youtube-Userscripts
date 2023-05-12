02a9e6f9d2105d1220a9f4c87a417765b86f78d4
Vincent Liem <96896686+VincentLiem@users.noreply.github.com>
2023-05-12 16:41:54 -0700

    Add files via upload

---
+++ Get RSS Feed From YouTube Channel.user.js
@@ -0,0 +1,34 @@
+// ==UserScript==
+// @name         Get RSS Feed From YouTube Channel
+// @description  Get RSS Feed from YouTube Channel, show in console
+// @version      1
+// @match        *.youtube.com/*
+// ==/UserScript==
+
+(function () {
+
+    //Username method
+    const usernameMatch =
+        window.location.pathname.match(/^\/user\/([^/]+)/) //youtube.com/users/name format
+        || window.location.pathname.match(/^\/c\/([^/]+)/) //youtube.com/c/name format
+        || window.location.pathname.match(/^\/@([^/]+)/); //youtube.com/@name format
+    const username = usernameMatch ? usernameMatch[1] : null;
+
+    if (username) {
+        console.log(`RSS feed URL with Username -Error for some channels- : https://www.youtube.com/feeds/videos.xml?user=${username}`); //Some channels don't have RSS in this format
+    } else {
+        console.warn("No RSS feed with Username found.");
+    }
+
+
+    //Channel ID method
+    const channelIdMatch = document.documentElement.innerHTML.match(/"channelId":"([^"]+)"/);
+    const channelId = channelIdMatch ? channelIdMatch[1] : null;
+
+    if (channelId) {
+        // Display the RSS feed URL for the channel in the console
+        console.log(`RSS feed URL with Channel ID: https://www.youtube.com/feeds/videos.xml?channel_id=${channelId}`);
+    } else {
+        console.warn("No RSS feed with Channel ID found.");
+    }
+})();
